4a701e87b1bac6243f950872b88cf7717d0e0fac
Yujun_Zhou <zhou100@illinois.edu>
2018-04-11 22:52:45 -0500

    rmd and running average

--- R/weather_data_clean.R
+++ R/weather_data_clean.R
@@ -12,7 +12,7 @@
 # Output: 
 # 0. growing degree days (both lhz and cluster level) 
 # 1. total rainfalls in growing seasons  （Growing season total precipitation）
-# 2. Number of no rain days    
+# 2. Number of max no rain days in a crop year
 # 3.   GDD - number of days where temp was between 8 to 32 C (Tmax + Tmin)/2 . Deschênes and Greenstone (2007) yield  on weather 
 
 ################################
@@ -46,6 +46,19 @@ month = strftime(date,"%m")
 month = as.numeric(month)
 year = strftime(date,"%Y")
 
+# generate cropyear 
+if ("2008-10-01"<precip_lhz_tz$date<"2009-05-01"){
+  precip_lhz_tz["cropyear_tz"] = 1 
+}
+
+precip_lhz_tz["cropyear_tz"] = ifelse(precip_lhz_tz$month>= 8 & precip_lhz_tz$date <="2009-05-01",1,0)
+
+
+#boulder_daily_precip <- boulder_daily_precip %>%
+#  mutate(DATE = as.Date(DATE, format = "%m/%d/%y"))
+
+
+
 precip_lhz_tz["year"] = year
 precip_lhz_tz["date"] = date
 precip_lhz_tz["month"] = month
@@ -62,9 +75,57 @@ precip_clust_tz["year"] = year
 precip_clust_tz["date"] = date
 precip_clust_tz["month"] = month
 
+
+
+# 1. generate crop year, so that it's summing up by crop year 
+# 2. transmute 
+# rain to moving sums of daily rain -10 >0
+# 2. arrange(desc()) to sort
+# mutate_all(dplyr::between(-1,0.5))
+# 3. use moving sums of rain >0, 
+# 4. first(date[.>0]
+
+
 ########################################################################  
-# generate the first date of rain after October month >=10 each year 
+# generate the first date of rain after October for Tanzania and After April for Uganda 
+# day1rain "the number of days after Oct 1 where five-day rainfall > 10 and it rained at least 3/5 days"
 ######################################################################## 
+library(dplyr)
+
+# moving mean for that day and previous days (e.g. 5 represents the mean of that day and the for previous days)
+# df2 = df %>%
+#   group_by(site, year) %>%
+#   arrange(site, year, day) %>%
+#   mutate(temp.5 = rollmean(x = temp, 5, align = "right", fill = NA))
+# head(df2, 75)
+# 
+# # moving mean for the previous days not including the current day (e.g. 5 represents the mean of the 5 previous days)
+# df2 = df2 %>%
+#   mutate(temp.lag1 = lag(temp, n = 1)) %>%
+#   mutate(temp.5.previous = rollapply(data = temp.lag1, 
+#                                      width = 5, 
+#                                      FUN = mean, 
+#                                      align = "right", 
+#                                      fill = NA, 
+#                                      na.rm = T))
+# head(df2, 75)
+
+# df2 = df2 %>%
+#   mutate(precip.30 = rollsum(x = precip, 30, align = "right", fill = NA))
+# head(df2, 75)
+
+
+# create lag weather
+precip_lhz_tz %>%
+  group_by(year) %>%
+  dplyr::mutate_all(funs(lag(.,n=12,order_by = date)))
+
+
+precip_boulder_AugOct <- boulder_daily_precip %>%
+  filter(DATE >= as.Date('2013-08-15') & DATE <= as.Date('2013-10-15'))
+
+
+
 
 day1rain_lhz_tz = precip_lhz_tz %>% 
   dplyr::filter(month>9) %>%  # planting/growing season starts from oct
@@ -90,4 +151,21 @@ day1rain_clust_ug = precip_clust_ug %>%
   dplyr::group_by(year) %>%   # calculate for each year individually
   dplyr::summarise_all(funs(first(date[.>0])))  # first date that has rainfall >0 
 
+write.csv(day1rain_clust_ug,"data/clean/rain/day1rain_clust_ug.csv")
+write.csv(day1rain_clust_tz,"data/clean/rain/day1rain_clust_tz.csv")
+write.csv(day1rain_lhz_tz,"data/clean/rain/day1rain_lhz_tz.csv")
+write.csv(day1rain_lhz_ug,"data/clean/rain/day1rain_lhz_ug.csv")
 
+# where five-day rainfall > 10 and it rained at least 3/5 days"
+
+#################################################################################
+#### generate maxdaysno rain 
+########### longest dry spell during the rainy season (Oct-Mar) per crop year (May-Apr)"
+#################################################################################
+day1rain_lhz_ug
+
+
+
+#rain_cytot = rain_Oct+rain_Nov+rain_Dec+rain_Jan+rain_Feb+rain_Mar+rain_Apr
+
+# rain_cytot
